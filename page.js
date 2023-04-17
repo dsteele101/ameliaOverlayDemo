fc250e7fe3de9a729604ddebf47a7253b3b7d967
Dsteele <derrick.l.steele@gmail.com>
2023-04-16 19:47:03 -0500

    Update page.js

--- page.js
+++ page.js
@@ -40,7 +40,7 @@ function createFrame() {
     x.setAttribute("id", "parentFrame");
     x.setAttribute("src", url);
     x.setAttribute("style", "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:99;");
-    x.setAttrivute("sandbox", "");
+    x.setAttribute("sandbox", "");
     document.body.appendChild(x);
 }
 
