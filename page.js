9452404021fd54f88dcf1f87f8052cd6892003f8
Dsteele <derrick.l.steele@gmail.com>
2023-04-16 19:53:50 -0500

    Update page.js

--- page.js
+++ page.js
@@ -85,6 +85,7 @@ function loadiFrame(url) {
     x.setAttribute("src", url);
     x.setAttribute("id", "tempiFrame");
     x.setAttribute("style", "position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:99;");
+    x.setAttribute("sandbox", "allow-scripts allow-same-origin");
     document.body.appendChild(x);
 }
 
